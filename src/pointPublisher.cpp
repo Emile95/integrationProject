bd340c0ba8a58a8698ddc3b288f30c41679dbba3
Emile Desjardins <desjardins.e@outlook.com>
2020-09-17 19:08:48 -0400

    instanciate message test, change triangle_point msg strucutre

--- src/pointPublisher.cpp
+++ src/pointPublisher.cpp
@@ -23,9 +23,29 @@ int main(int argc, char **argv)
         std::string input;
         std::cin >> input;
 
-        integrationTest::triangle_point points;
+        integrationTest::triangle_point msg;
 
-        threePointPublisher.publish(points);
+        //msg.triangle_point_ = new geometry_msgs::Point[] { { 1.0, 1.0, 1.0 }, { 2.0, 2.0, 2.0 }, { 3.0, 3.0, 3.0 } };
+
+        geometry_msgs::Point p1;
+        p1.x = 1.0;
+        p1.y = 1.0;
+        p1.z = 1.0;
+        msg.a = p1;
+
+        geometry_msgs::Point p2;
+        p2.x = 1.0;
+        p2.y = 1.0;
+        p2.z = 1.0;
+        msg.b = p2;
+
+        geometry_msgs::Point p3;
+        p3.x = 1.0;
+        p3.y = 1.0;
+        p3.z = 1.0;
+        msg.c = p3;
+
+        threePointPublisher.publish(msg);
     }
 
     return 0;

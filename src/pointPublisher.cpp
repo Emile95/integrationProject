9934fba23983f4b89fe1f4e8b41069bb3085d1c5
Emile Desjardins <desjardins.e@outlook.com>
2020-09-17 23:23:57 -0400

    simplify triangle point publisher

--- src/pointPublisher.cpp
+++ src/pointPublisher.cpp
@@ -3,46 +3,39 @@
 #include "geometry_msgs/Point.h"
 #include "integrationTest/triangle_point.h"
 
-#include <string>
-
-#include <iostream>
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "point_publisher");
-    ros::NodeHandle n("~");
+    ros::NodeHandle n;
 
-    //three_point topic publisher
     ros::Publisher threePointPublisher = n.advertise<integrationTest::triangle_point>("three_point", 1000);
 
     while(ros::ok())
     {
-        //ROS_INFO("%s", msg.data.c_str());
-        std::cout << "give three 3D point : [ 0.0 , 0.0, 0.0 ] , [ 1.0, 1.0, 1.0 ] , [ 2.0, 2.0, 2.0 ]" << std::endl;
-
+        std::cout << "type 'send' to publish triangle 3D point" << std::endl;
         std::string input;
         std::cin >> input;
 
+        if(input != "send") continue;
+
         integrationTest::triangle_point msg;
-
-        //msg.triangle_point_ = new geometry_msgs::Point[] { { 1.0, 1.0, 1.0 }, { 2.0, 2.0, 2.0 }, { 3.0, 3.0, 3.0 } };
-
+        
         geometry_msgs::Point p1;
-        p1.x = 1.0;
-        p1.y = 1.0;
-        p1.z = 1.0;
+        p1.x = 5.23;
+        p1.y = 7.678;
+        p1.z = 8.689;
         msg.a = p1;
 
         geometry_msgs::Point p2;
-        p2.x = 1.0;
-        p2.y = 1.0;
-        p2.z = 1.0;
+        p2.x = 1.4567;
+        p2.y = 10.23;
+        p2.z = 23.5;
         msg.b = p2;
 
         geometry_msgs::Point p3;
-        p3.x = 1.0;
-        p3.y = 1.0;
-        p3.z = 1.0;
+        p3.x = 5.78;
+        p3.y = 3.23;
+        p3.z = 7.67;
         msg.c = p3;
 
         threePointPublisher.publish(msg);

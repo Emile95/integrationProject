41f748962fb98f0c5cc6904e87854953d6f46929
Emile Desjardins <desjardins.e@outlook.com>
2020-09-22 02:19:12 -0400

    factorize console input for 3d points and

--- src/triangle3DPointTeleop.cpp
+++ src/triangle3DPointTeleop.cpp
@@ -8,14 +8,22 @@
 
 bool consoleAskDimensionValue(const std::string& coutStr, double& number)
 {
-    std::cout << coutStr << std::endl;
-
+    std::cout << coutStr;
     std::string input;
     std::cin >> input;
 
     number = atof(input.c_str());
 
     return ros::ok();
+}
+
+bool consoleAsk3dDPoint(const std::string& pointName, geometry_msgs::Point& point) 
+{
+    std::string consoleOut = "Point " + pointName +  ", dimension ";
+    if(!consoleAskDimensionValue(consoleOut + "x = ", point.x)) return false;
+    if(!consoleAskDimensionValue(consoleOut + "y = ", point.y)) return false;
+    if(!consoleAskDimensionValue(consoleOut + "z = ", point.z)) return false;
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -36,41 +44,29 @@ int main(int argc, char **argv)
         //Triangle 3d point message to publish
         integrationTest::triangle_point trianglePointMsg;
 
-        //3d point A
-        geometry_msgs::Point a;
-        if(!consoleAskDimensionValue("Point A, dimension x = ", a.x)) break;
-        if(!consoleAskDimensionValue("Point A, dimension y = ", a.y)) break;
-        if(!consoleAskDimensionValue("Point A, dimension z = ", a.z)) break;
-        trianglePointMsg.a = a;
-
-        //3d point B
-        geometry_msgs::Point b;
-        if(!consoleAskDimensionValue("Point B, dimension x = ", b.x)) break;
-        if(!consoleAskDimensionValue("Point B, dimension y = ", b.y)) break;
-        if(!consoleAskDimensionValue("Point B, dimension z = ", b.z)) break;
-        trianglePointMsg.b = b;
-
-        //3d point C
-        geometry_msgs::Point c;
-        if(!consoleAskDimensionValue("Point C, dimension x = ", c.x)) break;
-        if(!consoleAskDimensionValue("Point C, dimension y = ", c.y)) break;
-        if(!consoleAskDimensionValue("Point C, dimension z = ", c.z)) break;
-        trianglePointMsg.c = c;
+        //Ask for 3 3D points
+        //Handle ctrl+c between dimension of each points
+        if(!consoleAsk3dDPoint("A", trianglePointMsg.a)) break;
+        if(!consoleAsk3dDPoint("B", trianglePointMsg.b)) break;
+        if(!consoleAsk3dDPoint("C", trianglePointMsg.c)) break;
 
         threePointPublisher.publish(trianglePointMsg);
+        std::cout << "publish triangle 3d points on /triangle_3d_points topic" << std::endl;
         
         //Create triangle_area_resolver srv with triangle_3d_points msg
         integrationTest::triangle_area_resolver srv;
-        srv.request.a = a;
-        srv.request.b = b;
-        srv.request.c = c;
+        srv.request.a = trianglePointMsg.a;
+        srv.request.b = trianglePointMsg.b;
+        srv.request.c = trianglePointMsg.c;
         
         if (triangleAreaResolverclient.call(srv))
         {
+            std::cout << "publish triangle area on /triangle_area topic" << std::endl;
             integrationTest::triangle_area triangleAreaMsg;
             triangleAreaMsg.area = srv.response.area;
             triangleAreaPublisher.publish(triangleAreaMsg);
         }
+        std::cout << "---------------------------" << std::endl;
     }
 
     return 0;
